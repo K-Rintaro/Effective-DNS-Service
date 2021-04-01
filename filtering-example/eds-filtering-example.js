039c18e340096dd3123d67aff448c1550877f39d
K-Rintaro <git.rintaro@gmail.com>
2021-04-01 13:53:22 +0900

    add filtering exmaple

---
+++ filtering-example/eds-filtering-example.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const helmet = require('helmet')
+
+const port = process.env.PORT || 3000;
+
+app.use(helmet())
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.listen(port, ()=> {
+  console.log(`EDS listening on port ${port}!`);
+});
+
+//[example: web filtering black list]
+const blacklist = ["www.example.com", "www.test.com", "aqua-and-pink.herokuapp.com"]
+
+app.post('/dog', function(req, res, next) {
+    const FQDN = req.body.fqdn;
+    (async () => {
+      const doh = require('@sagi.io/dns-over-https')
+      const dnsResponse  = await doh.query({name: FQDN})
+      const dnsip = dnsResponse.answers
+      const kakunouko = []
+      for (let i = 0; i < dnsip.length; i++){
+          kakunouko.push({record: dnsip[i].type , addresss: dnsip[i].data});
+      }
+      if (blacklist.includes(FQDN) == false) {
+        res.send(kakunouko);
+        console.log(kakunouko);
+      }else{
+        res.status(403);
+        res.send({code: 403, error: "EDS sever blocked DNS request because the FQDN was blacklisted."})
+        console.log(403)
+      }
+    })()
+});
+
+app.post('/cat', function(req, res, next) {
+  const FQDN = req.body.fqdn;
+  dns.lookup(FQDN, function (err, addresses, family) {
+      res.send('sample connection was successed!')
+  });
+});
